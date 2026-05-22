3ce50bc63ff8b2a3929ad9461f4484fd7094bea2
Dmitry Stolyarov <Dmitry221556@yandex.ru>
2026-05-22 10:26:08 +0700

    lab6

--- embedding_system.py
+++ embedding_system.py
@@ -3,8 +3,18 @@ import re
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
+import json
+from openai import OpenAI
+from myenv import API_KEY
 
 EMBEDDING_MODEL = SentenceTransformer("sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
+
+client = OpenAI(
+    api_key=API_KEY,
+    base_url="https://polza.ai/api/v1"
+)
+LLM_MODEL = "deepseek/deepseek-v4-flash"
+
 
 #Lab3 ===========
 
@@ -43,3 +53,75 @@ def cos_compare(a: np.ndarray, b: np.ndarray):
     similarity_matrix = cosine_similarity(a_2d, b_2d)
 
     return float(similarity_matrix[0, 0])
+
+#Lab6 ===========
+
+def get_ontology_data_vectors(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    nodes = data.get('nodes', [])
+    arcs = data.get('arcs', [])
+    node_data = {}
+
+    for node in nodes:
+        id = node.get('id')
+        params = node.get('data', {}).get('params_values', {})
+        labels = params.get('http://www.w3.org/2000/01/rdf-schema#label',  ['Нет названия'])
+        name = labels[0].split('@')[0]
+        desc = params.get('http://www.w3.org/2000/01/rdf-schema#comment', 'Нет описания')
+        node_data[id] = {'name': name, 'desc': desc, 'relations': []}
+
+    for arc in arcs:
+        source_id = arc.get('source')
+        target_id = arc.get('target')
+        uri = arc.get('data', {}).get('uri', '')
+
+        if source_id in node_data and target_id in node_data:
+            if uri in node_data:
+                rel_name = node_data[uri]['name']
+            elif '#' in uri:
+                rel_name = uri.split('#')[-1]
+            else:
+                rel_name = uri.split('/')[-1]
+
+            target_name = node_data[target_id]['name']
+            node_data[source_id]['relations'].append(f"{rel_name}: {target_name}")
+
+    ans = []
+    for id, dt in node_data.items():
+        lines = [f"Название: {dt['name']}", f"Описание: {dt['desc']}"]
+        lines.extend(dt['relations'])
+
+        node_text = "\n".join(lines)
+        embedding = get_embeddings([node_text])[0]
+
+        ans.append({'id': id, 'text': node_text, 'embedding': embedding})
+    return ans
+
+
+def make_prompt(user_request, ontology_data):
+    prompt = f"Дай ответ на данный вопрос, используя информацию из текста:\n{user_request}\nТекст:\n{ontology_data}"
+    response = client.chat.completions.create(
+        model=LLM_MODEL,
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content
+
+def iterate_llm(user_request, ontology_data, helping_nodes_saved=[], helping_nodes_new=3):
+    request_embedding = get_embeddings([user_request])[0]
+    for el in ontology_data:
+        el['score'] = cos_compare(el['embedding'], request_embedding)
+    ontology_data.sort(key=lambda x: x['score'], reverse=True)
+    saved_ids = set([node['id'] for node in helping_nodes_saved])
+
+    for node in ontology_data[:helping_nodes_new]:
+        if node['id'] not in saved_ids:
+            helping_nodes_saved.append(node)
+    context_text = ""
+    for node in helping_nodes_saved:
+        context_text += node['text'] + "\n\n"
+
+    answer = make_prompt(user_request, context_text)
+    return answer
+
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from embedding_system import *
 
+ONTOLOGY_PATH = "graph.json"
 
-def test_embedding_pipeline(
-        test_text: str = None,
-        chunk_size: int = 64,
-):
-    if test_text is None:
-        test_text = """ Маша живет в Новосибирске. Маша изучает информационные технологии.
-        
-        В Новосибирском государственном университете есть факультет информационных технологий"""
-    chunks = get_chunks(test_text)
-    print(chunks)
-    embeddings = get_embeddings(chunks)
-    print(embeddings)
-    sim1 = cos_compare(embeddings[0], embeddings[1])
-    print(sim1)
+def rag_implementation(request):
+    #Фаза 1
+    ontology_data = get_ontology_data_vectors(ONTOLOGY_PATH)
+
+    #Фаза 2
+    saved_nodes = []
+    answer = iterate_llm(request, ontology_data, saved_nodes)
+
+    #Фаза 3
+    answer = iterate_llm(answer, ontology_data, saved_nodes)
+    return answer
 
 if __name__ == "__main__":
-    test_embedding_pipeline()
+    pass
